3f2daa774d830d6235b646382625690f0e01cd61
meixuanoh <ohmx93@gmail.com>
2019-12-10 19:33:47 +0800

    custom keyboard v1

--- chatbot/chatbot.rb
+++ chatbot/chatbot.rb
@@ -18,18 +18,25 @@ end
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
-    # print message
-    case message.text
-    when '/start'
-      bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
-    when '/stop'
-      bot.api.send_message(chat_id: message.chat.id, text: "Bye, #{message.from.first_name}")
-    when '/quote'
-      bot.api.send_message(chat_id: message.chat.id, text: getquote())
-    when '/joke'
-      bot.api.send_message(chat_id: message.chat.id, text: getjoke())
+    case message
+    when Telegram::Bot::Types::CallbackQuery
+      # Here you can handle your callbacks from inline buttons
+      if message.data == 'quote'
+        bot.api.send_message(chat_id: message.from.id, text: getquote())
+      elsif message.data == 'joke'
+        bot.api.send_message(chat_id: message.from.id, text: getjoke())
+      end
+    when Telegram::Bot::Types::Message
+      kb = [
+        Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Inspire me!', callback_data: 'quote'),
+        Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Joke of the day', callback_data: 'joke'),
+      ]
+      markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
+      bot.api.send_message(chat_id: message.chat.id, text: "How can I help you #{message.from.first_name}?", reply_markup: markup)
     end
   end
 end
+
+
 
 
