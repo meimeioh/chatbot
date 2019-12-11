c72358d5570c15c3b6a41e56efeaca3e1b1061ae
meixuanoh <ohmx93@gmail.com>
2019-12-11 16:29:30 +0800

    weather forecast v1

--- chatbot/trivia.rb
+++ chatbot/trivia.rb
@@ -30,7 +30,7 @@ Telegram::Bot::Client.run(token) do |bot|
     when Telegram::Bot::Types::Message
       kb = [
         Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Inspire me!', callback_data: 'quote'),
-        Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Joke of the day', callback_data: 'joke'),
+        Telegram::Bot::Types::InlineKeyboardButton.new(text: 'Joke of the day', callback_data: 'joke')
       ]
       markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
       bot.api.send_message(chat_id: message.chat.id, text: "How can I help you #{message.from.first_name}?", reply_markup: markup)
--- chatbot/weather.rb
+++ chatbot/weather.rb
@@ -4,22 +4,43 @@ require 'httparty'
 
 token = ENV["BOT_API_KEY"]
 
+def get_weather(lat, long)
+  weather = ENV["WEATHER_API_KEY"]
+  url = "https://api.darksky.net/forecast/#{weather}"
+  update_location = "#{url}/#{lat},#{long}"
+  response = HTTParty.get(update_location)
+  "Right now: #{response['currently']['summary']}
+Today: #{response['hourly']['summary']}
+This week: #{response['daily']['summary']}"
+end
+
+puts get_weather(1.3521, 103.8198)
+
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message
     when Telegram::Bot::Types::CallbackQuery
       # Here you can handle your callbacks from inline buttons
       if message.data == 'weather'
-        kb = [Telegram::Bot::Types::KeyboardButton.new(text: 'Show me your location', request_location: true)]
-        markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb)
+        kb = [Telegram::Bot::Types::KeyboardButton.new(text: 'Send your location!', request_location: true)]
+        markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb, resize_keyboard: true, one_time_keyboard: true)
         bot.api.send_message(chat_id: message.from.id, text: 'Where are you?', reply_markup: markup)
       end
     when Telegram::Bot::Types::Message
-      kb = [
-        Telegram::Bot::Types::InlineKeyboardButton.new(text: 'What\'s the weather today?', callback_data: 'weather'),
-      ]
-      markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
-      bot.api.send_message(chat_id: message.chat.id, text: "How can I help you #{message.from.first_name}?", reply_markup: markup)
+      # puts "From #{message.from.id}"
+      # puts "Chats #{message.chat.id}"
+      # puts message.location == nil
+      if !message.location.nil?
+        lat = message.location.latitude
+        long = message.location.longitude
+        bot.api.send_message(chat_id: message.chat.id, text: get_weather(lat, long))
+      else
+        kb = [
+          Telegram::Bot::Types::InlineKeyboardButton.new(text: 'What\'s the weather today?', callback_data: 'weather'),
+        ]
+        markup = Telegram::Bot::Types::InlineKeyboardMarkup.new(inline_keyboard: kb)
+        bot.api.send_message(chat_id: message.chat.id, text: "How can I help you #{message.from.first_name}?", reply_markup: markup)
+      end
     end
   end
 end
